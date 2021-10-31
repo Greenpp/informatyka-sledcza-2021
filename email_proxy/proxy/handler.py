f04ad776804c12a4f88fcc15d51085333e9e440b
Jakub Ciszek <jakubciszek64@gmail.com>
2021-10-31 20:44:41 +0100

    Receiver/quarantine routing

--- email_proxy/proxy/handler.py
+++ email_proxy/proxy/handler.py
@@ -1,6 +1,13 @@
 import logging
+from smtplib import SMTP
 
 from ..handler import Handler
+from ..settings import (
+    QUARANTINE_HOST,
+    QUARANTINE_PORT,
+    TARGET_HOST,
+    TARGET_PORT,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -8,16 +15,29 @@ logger = logging.getLogger(__name__)
 class AnalyzingProxyHandler(Handler):
     async def handle_DATA(self, server, session, envelope) -> str:
         logger.debug(f'Data received in proxy')
-        
+
+        logger.debug('Sending email to receiver')
+        self._send_to_receiver(envelope)
+        logger.debug('Sending email to quarantine')
+        self._send_to_quarantine(envelope)
+
         return '250 OK'
 
-    def _send_to_receiver(self) -> None:
-        # TODO pass to receiver
-        pass
+    def _send_to_receiver(self, envelope) -> None:
+        with SMTP(TARGET_HOST, TARGET_PORT) as client:
+            client.sendmail(
+                envelope.mail_from,
+                envelope.rcpt_tos,
+                envelope.original_content,
+            )
 
-    def _send_to_quarantine(self) -> None:
-        # TODO pass to quarantine
-        pass
+    def _send_to_quarantine(self, envelope) -> None:
+        with SMTP(QUARANTINE_HOST, QUARANTINE_PORT) as client:
+            client.sendmail(
+                envelope.mail_from,
+                envelope.rcpt_tos,
+                envelope.original_content,
+            )
 
 
 # NOTE build-in simple proxy handler
