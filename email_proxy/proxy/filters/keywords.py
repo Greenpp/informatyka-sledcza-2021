e11d4ce1d7c4ca8ca0c4aa9b46f989e229399157
Jakub Ciszek <jakubciszek64@gmail.com>
2021-12-13 11:45:51 +0100

    Variable fixed

--- email_proxy/proxy/filters/keywords.py
+++ email_proxy/proxy/filters/keywords.py
@@ -4,14 +4,25 @@ from .filter import Filter
 
 
 class KeywordsFilter(Filter):
-    """ Checks emails based on spam words"""
+    """Checks emails based on spam words"""
 
     file_path = TRIGGER_WORDS_FILE
 
     def is_spam_or_dangerous(self, email: Email) -> bool:
-        
-        message = Email.msg.lower().replace('.', '').replace('!', '').replace('?', '').split(' ')
-        subject = Email.subject.lower().replace('.', '').replace('!', '').replace('?', '').split(' ')
+        message = (
+            email.msg.lower()
+            .replace('.', '')
+            .replace('!', '')
+            .replace('?', '')
+            .split(' ')
+        )
+        subject = (
+            email.subject.lower()
+            .replace('.', '')
+            .replace('!', '')
+            .replace('?', '')
+            .split(' ')
+        )
         words_list = self._read_from_file(self.file_path)
 
         result = any(map(lambda x: x in message or x in subject, words_list))
@@ -22,7 +33,7 @@ class KeywordsFilter(Filter):
 
         with open(path, 'r') as f:
             trigger_words = f.read().split(',')
-        
+
         trigger_words = [word.lower() for word in trigger_words]
 
         return trigger_words
