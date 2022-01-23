fc1476541d8feb3850c04523e8917acfcf013462
Jakub Brzoza <63004576+Qbsoonn@users.noreply.github.com>
2022-01-23 17:06:26 +0100

    Docstrings

--- email_proxy/quarantine/handler.py
+++ email_proxy/quarantine/handler.py
@@ -7,6 +7,8 @@ logger = logging.getLogger(__name__)
 
 
 class QuarantineHandler(Handler):
+    """Quarantine specific handler"""
+    
     async def handle_DATA(self, server, session, envelope) -> str:
         email = Email.from_envelope(envelope)
         logger.info(f'Data received in quarantine | {email=}')
